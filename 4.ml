d6c9d1b7c1ae866687c8706e1c3225cd5c77795f
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-03-07 01:36:49 +0900

    Create 4.ml

---
+++ 4.ml
@@ -0,0 +1,6 @@
+let rec pos n =
+        let neg n =
+                if n<0 then 0.
+                else pos n -. 1.0 /. (float_of_int (4 * n +3 ))
+        in
+        neg (n - 1) +. 1. /. (float_of_int (4 * n +1 ))
