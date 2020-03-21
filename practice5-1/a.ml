598066f34bd0f434bb851910e38cb8c43e36436b
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-03-21 13:00:24 +0900

    Create a.ml

---
+++ practice5-1/a.ml
@@ -0,0 +1,8 @@
+(*
+1.正しい　型は'a list list
+2.正しくない　前はint list 後ろはstring list
+3.正しい　型はint list lsit
+4.正しくない　右結合で[3]::[] はint list list型　2はint list型
+5.正しい　型は'a list list 
+6.正しい　型は (bool -> bool) list
+*)
