39a6756fc3f61ddeb3a2a680504a6e0e8dd0edf0
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-03-21 13:29:39 +0900

    Update 2.ml

--- practice5-2/2.ml
+++ practice5-2/2.ml
@@ -3,3 +3,9 @@ let rec roman romlist n =
         [] -> ""
         | (x,y) :: rest when x <= n -> y ^ (roman ((x,y)::rest)(n-x))
         | (x,y) :: rest -> (roman rest n)
+
+let rec roman romlist n =
+        match romlist with
+        [] -> ""
+        | (x,y) :: rest -> if x <= n then y ^ (roman ((x,y)::rest)(n-x))
+                                     else (roman rest n)
