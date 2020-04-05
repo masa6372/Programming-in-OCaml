d28b53eb096d9a2ace458c0487be9d781aa3b770
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-04-05 18:39:58 +0900

    Update 3.ml

--- practice3-6/3.ml
+++ practice3-6/3.ml
@@ -1,4 +1,4 @@
 (*任意の整数x,yに対し、f(sum_and_diff(x,y))が(x,y)を返すような関数*)
-let sum_and_diff x y = (x+y,x-y);;
+let sum_and_diff (x,y) = (x+y,x-y);;
 
-let f x y = ((x+y)/2,(x-y)/2);;
+let f (x,y) = ((x+y)/2,(x-y)/2);;
