8514cfb8d241c4e849049f55a7b5ddf0d1c0bc9d
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-04-30 00:51:05 +0900

    Update 7.ml

--- practice5-2/7.ml
+++ practice5-2/7.ml
@@ -2,3 +2,10 @@ let rec filter p l =
         match l with
         [] -> []
         | x :: rest -> if p x then (x :: filter p rest) else filter p rest
+
+let filter func l1 =
+	let rec f func l2 = function
+		[] -> l2 
+		| x :: rest -> if (func x) then f func (x :: l2) rest
+				else f func l2 rest
+	in List.rev(f func [] l1) 
