68428889027ea6366f04745a07389b66f9301c4a
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-05-01 01:52:14 +0900

    Update 8.ml

--- 99problems/8.ml
+++ 99problems/8.ml
@@ -5,3 +5,8 @@ let compress l1 =
 	| a :: (b :: _ as t) ->  if a = b then f l2 t
 				 else f (a :: l2) t
     in  List.rev (f [] l1)
+    
+    
+let rec compress = function
+    | a :: (b :: _ as t) -> if a = b then compress t else a :: compress t
+    | x -> x
