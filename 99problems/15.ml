7cca33e9705cfd50af51ac1f25b591318548852a
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-05-12 18:47:37 +0900

    Update 15.ml

--- 99problems/15.ml
+++ 99problems/15.ml
@@ -7,3 +7,11 @@ let replicate list num2 =
   | x :: (a :: _ as rest) -> if (num1 < num2) then func (num1+1) num2 (x :: acc) list
 			     else func 1 num2 (a :: acc) rest
   in List.rev (func 0 num2 [] list)
+
+let replicate list n =
+  let rec prepend n acc x =
+    if n = 0 then acc else prepend (n-1) (x :: acc) x in
+  let rec aux acc = function  
+    | [] -> acc
+    | h :: t -> aux (prepend n acc h) t in
+  aux [] (list.rev list)
