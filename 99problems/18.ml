ef272763f0246b460fa4e6592afd85981c67a565
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-05-17 22:18:23 +0900

    Update 18.ml

--- 99problems/18.ml
+++ 99problems/18.ml
@@ -4,3 +4,15 @@ let slice list i j =
     | x :: rest -> if tmp1>=i && tmp1<=j then x::aux (tmp1+1) rest
 		   else aux (tmp1+1) rest
   in aux 0 list
+
+
+let slice list i k =
+  let rec take n = function
+    | [] -> []
+    | h :: t -> if n=0 then [] else h :: take (n-1) t
+  in
+  let rec drop n = function
+    | [] -> []
+    | h :: t as l -> if n=0 then l else drop (n-1) t
+  in
+  take (k-i+1) (drop i list)
